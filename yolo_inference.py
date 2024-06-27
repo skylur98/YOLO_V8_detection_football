637d7dbdd0d58a5913cebb69aede1cba849f8734
skylur98 <skylur20@gmail.com>
2024-06-27 19:22:41 +0900

    model check

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,1 +1,12 @@
-print("hi")
+from pyexpat import model
+from ultralytics import YOLO
+
+
+model = YOLO('yolov8x')
+
+results = model.predict('input_videos/test_1.mp4', save=True)
+
+print(results[0])
+print("=======================")
+for box in results[0].boxes:
+    print(box)
