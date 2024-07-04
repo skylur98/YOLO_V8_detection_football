0edb9b82945e7cdadc25dfe82002d4f87e970942
skylur98 <skylur@naver.com>
2024-07-04 17:14:53 +0900

    update tracker

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ def main():
     # Initialize Tracker
     tracker = Tracker('models/best.pt')
 
-    tracks = tracker.get_object_tracks(video_frames)
+    tracks = tracker.get_object_tracks(video_frames,
+                                       read_from_stub=True,
+                                       stub_path='stubs/track_stubs.pk1')
 
     #Save Video
     save_video(video_frames, 'output_videos/output_video.avi')
--- trackers/tracker.py
+++ trackers/tracker.py
@@ -1,5 +1,6 @@
 from ultralytics import YOLO
 import supervision as sv
+import pickle
 
 class Tracker:
     def __init__(self,model_path):
@@ -12,10 +13,14 @@ class Tracker:
         for i in range(0,len(frames),batch_size):
             detections_batch = self.model.predict(frames[i:i+batch_size],conf=0.1)
             detections += detections_batch
-            break
         return detections
     
-    def get_object_tracks(self, frames):
+    def get_object_tracks(self, frames, read_from_stub=False, stub_path=None):
+
+        if read_from_stub and stub_path is not None and os.path.exists(stub_path):
+            with open(stub_path,'rb') as f:
+                tracks = pickle.load(f)
+            return tracks
 
         detections = self.detect_frames(frames)
 
@@ -47,7 +52,36 @@ class Tracker:
 
             for frame_detection in detection_with_tracks:
                 bbox = frame_detection[0].tolist()
-                                  
-            print(detection_with_tracks)
+                cls_id = frame_detection[3]
+                track_id = frame_detection[4]
 
-            
+                if cls_id == cls_names_inv['player']:
+                    tracks["players"][frame_num][track_id] = {"bbox":bbox}
+
+                if cls_id == cls_names_inv['referee']:
+                    tracks["referees"][frame_num][track_id] = {"bbox":bbox}
+
+            for frame_detection in detection_supervision:
+                bbox = frame_detection[0].tolist()
+                cls_id = frame_detection[3]
+
+                if cls_id == cls_names_inv['ball']:
+                    tracks["ball"][frame_num][1] = {"bbox":bbox}
+
+            if stub_path is not None:
+                with open(stub_path,'wb') as f:
+                    pickle.dump(tracks,f)
+                                  
+            return tracks
+
+    def draw_annotations(self,video_frames, tracks):
+        output_video_frames = []
+        for frame_num, frame in enumerate(video_frames):
+            frame = frame.copy()
+
+            player_dict = tracks["players"][frame_num]
+            ball_dict = tracks["ball"][frame_num]
+            referee_dict = tracks["referees"][frame_num]
+
+            for track_id, player in player_dict.items():
+                frame = self.draw_ellipse(frame, player["bbox"],(0,0,255),track_id)
