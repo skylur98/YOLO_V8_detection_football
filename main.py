89d13d2d3208f3ab8400e261c67d1cb1c92677c4
skylur98 <skylur@naver.com>
2024-07-12 17:22:39 +0900

    .

--- main.py
+++ main.py
@@ -27,6 +27,13 @@ def main():
     # Get object positions 
     tracker.add_position_to_tracks(tracks)
 
+     # camera movement estimator
+    camera_movement_estimator = CameraMovementEstimator(video_frames[0])
+    camera_movement_per_frame = camera_movement_estimator.get_camera_movement(video_frames,
+                                                                                read_from_stub=True,
+                                                                                stub_path='stubs/camera_movement_stub.pkl')
+    camera_movement_estimator.add_adjust_positions_to_tracks(tracks,camera_movement_per_frame)
+
     #draw_annotations / sample
     # tracker = tracker.get_object_tracks(video_frames,read_from_stub=False,
                                         
