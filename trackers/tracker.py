d2b0448d906b324e407fc23a5ae2519a7f7e8472
skylur98 <skylur@naver.com>
2024-07-24 06:59:54 +0900

    .

--- trackers/tracker.py
+++ trackers/tracker.py
@@ -23,8 +23,9 @@ class Tracker:
                     else:
                         position = get_foot_position(bbox)
                     tracks[object][frame_num][track_id]['position'] = position
+    
 
-     def interpolate_ball_positions(self,ball_positions):
+    def interpolate_ball_positions(self,ball_positions):
         ball_positions = [x.get(1,{}).get('bbox',[]) for x in ball_positions]
         df_ball_positions = pd.DataFrame(ball_positions,columns=['x1','y1','x2','y2'])
 
@@ -33,6 +34,8 @@ class Tracker:
         df_ball_positions = df_ball_positions.bfill()
 
         ball_positions = [{1: {"bbox":x}} for x in df_ball_positions.to_numpy().tolist()]
+
+        return ball_positions
     
 
     def detect_frames(self, frames):
